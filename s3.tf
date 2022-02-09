86f73e778342837a5eeded33580c4a718b1c3e72
Vladimir Samoylov <cageyv.dev@gmail.com>
2022-02-09 22:45:18 +0700

    update external modules, try to solve S3 issue

--- s3.tf
+++ s3.tf
@@ -1,6 +1,21 @@
+data "aws_iam_policy_document" "s3_policy" {
+  count = var.create ? 1 : 0
+  statement {
+    sid     = "AllowCloudFrontReadObjects"
+    actions = ["s3:GetObject"]
+    principals {
+      type        = "AWS"
+      identifiers = module.cloudfront.cloudfront_origin_access_identity_iam_arns
+    }
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}",
+    ]
+  }
+}
+
 module "s3_bucket" {
   source        = "terraform-aws-modules/s3-bucket/aws"
-  version       = "2.7.0"
+  version       = "2.13.0"
   create_bucket = var.create
   bucket        = var.s3_bucket_name
   acl           = "private"
@@ -18,58 +33,21 @@ module "s3_bucket" {
       }
     }
   }
-  attach_public_policy = false
 
-  tags = var.tags
-}
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.s3_policy[0].json
 
-data "aws_iam_policy_document" "s3_policy" {
-  count = var.create ? 1 : 0
-  statement {
-    sid     = "AllowCloudFrontReadObjects"
-    actions = ["s3:GetObject"]
-    principals {
-      type        = "AWS"
-      identifiers = module.cloudfront.cloudfront_origin_access_identity_iam_arns
-    }
-    resources = ["${module.s3_bucket.s3_bucket_arn}/*"]
-  }
-  statement {
-    sid     = "denyInsecureTransport"
-    effect  = "Deny"
-    actions = ["s3:*"]
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    resources = [
-      module.s3_bucket.s3_bucket_arn,
-      "${module.s3_bucket.s3_bucket_arn}/*",
-    ]
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values = [
-        "false"
-      ]
-    }
-  }
-}
+  attach_deny_insecure_transport_policy = true
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  count  = var.create ? 1 : 0
-  bucket = module.s3_bucket.s3_bucket_id
-  policy = data.aws_iam_policy_document.s3_policy[0].json
-}
-
-resource "aws_s3_bucket_public_access_block" "this" {
-  # Chain resources (s3_bucket -> s3_bucket_policy -> s3_bucket_public_access_block)
-  # to prevent "A conflicting conditional operation is currently in progress against this resource."
-  count  = var.create ? 1 : 0
-  bucket = aws_s3_bucket_policy.bucket_policy[0].id
-
+  # S3 bucket-level Public Access Block configuration
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+
+  # S3 Bucket Ownership Controls
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+
+  tags = var.tags
 }
