b4b8ab82644ff9dcfc9e8d9f2c83f89c90e9331d
Vladimir Samoylov <cageyv.dev@gmail.com>
2022-02-22 13:21:52 +0700

    add example site and simple build_deploy.sh

---
+++ examples/complete/build_deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+terraform apply -auto-approve
+BUCKET=$(terraform output --raw -no-color s3_bucket_id)
+CLOUDFRONT=$(terraform output -raw -no-color cloudfront_distribution_id)
+cd example_site
+rm -rf out/
+yarn install && yarn build
+aws s3 sync --only-show-errors out/ "s3://$BUCKET/"
+INVALIDATION_ID=$(aws cloudfront create-invalidation --distribution-id "$CLOUDFRONT" --paths "/*" --query 'Invalidation.Id' --output text)
+# aws cloudfront wait invalidation-completed --distribution-id "$CLOUDFRONT" --id "$INVALIDATION_ID"
